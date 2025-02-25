5808f85fed3401c0340524b5829437ce3826204b
dimanevyarovich <mkrtchmkr@gmail.com>
2025-02-25 21:24:15 +0400

    homework has been done

--- person.py
+++ person.py
@@ -1,4 +1,11 @@
-class Person:
+class Dexter:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+    def say_monologue(self):
+        return print("My name is dexter...dexter morgan i dont know made me the way that i am but whatever it was, left a hollow place inside me."
+                     " People fake alot of human interactions, but i feel like i fake them all very well. "
+                     "thats my burden i guess."
+                     "But i will not blame my foster parents. Henry and Dorris Morgan did a wonderful job raising me... "
+                     "but they're both dead now. I didnt kill them honest")
